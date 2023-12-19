e832a3ba98add9d37ba11e9bceb2983078fa6dbd
alexhiraga <hiraga_alex@hotmail.com>
2023-12-19 20:59:29 -0300

    adjust for deploy

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: process.env.NODE_ENV === 'production'
+        ? import.meta.env.BASE_URL_PROD
+        : import.meta.env.BASE_URL_LOCAL,
     withCredentials: true
 })
