4b19f5d5901b0f5b5d2f1f060ee95e3ad7834242
alexhiraga <hiraga_alex@hotmail.com>
2023-12-19 23:34:14 -0300

    fix production baseurl

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
     baseURL: process.env.NODE_ENV === 'production'
-        ? import.meta.env.BASE_URL_PROD
+        ? 'https://daily-diet-api-90e5.onrender.com'
         : import.meta.env.BASE_URL_LOCAL,
     withCredentials: true
 })
