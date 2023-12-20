314bf254db5722df211865a07c356e3df8473fe3
alexhiraga <hiraga_alex@hotmail.com>
2023-12-19 23:37:54 -0300

    fix env config for axios

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: process.env.NODE_ENV === 'production'
-        ? 'https://daily-diet-api-90e5.onrender.com'
-        : import.meta.env.BASE_URL_LOCAL,
+    baseURL: import.meta.env.MODE === 'production'
+        ? import.meta.env.VITE_BASE_URL_PROD
+        : import.meta.env.VITE_BASE_URL_LOCAL,
     withCredentials: true
 })
